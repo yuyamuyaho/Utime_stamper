de45d50d14d65ef706169c45d42df6eb433253cc
wuhanvirus <62196278+wuhanvirus@users.noreply.github.com>
2021-06-22 02:20:39 +0900

    text

--- content.js
+++ content.js
@@ -22,6 +22,7 @@ var video;
 var comment;
 var b;
 var btn;
+var grad;
 
 function eval_time(str) {
   return parseInt(str.replace(/:[0-9]+/, "")) * 60 + parseInt(str.replace(/[0-9]+:/, ""));
@@ -31,6 +32,7 @@ function on_active() {
   runtime = document.getElementsByClassName('ytp-time-current')[0];
 
   h1 = document.getElementsByTagName('h1');
+  grad = document.getElementsByClassName('ytp-gradient-bottom')[0];
   video = document.getElementsByClassName('video-stream html5-main-video')[0];
   comment = document.getElementsByClassName('style-scope ytd-expander');
   b = {};
@@ -46,14 +48,15 @@ function on_active() {
       ++run;
       if (b[run]) {
         if (btn) {
-          h1[0].style['padding'] = '16px';
-          h1[0].style['font-weight'] = 'bold';
-          h1[0].style['font-size'] = '20px';
-          h1[0].style['background-color'] = '#a0a0a0';
-          h1[0].style['text-shadow'] = '2px 2px 3px rgba(255,255,255,0.2)';
+          h1[1].style['padding'] = '16px';
+          h1[1].style['font-weight'] = 'bold';
+          h1[1].style['font-size'] = '20px';
+          // h1[1].style['background-color'] = '#a0a0a0';
+          h1[1].style['text-shadow'] = '2px 2px 3px rgba(255,255,255,0.2)';
           btn = false;
         }
-        h1[0].innerText = b[run];
+        // grad.innerText = b[run];
+        h1[1].innerText = b[run];
       }
     }, 1000);
     video.onpause = function () {
